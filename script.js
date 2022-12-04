8aed6909374314325e07af3c0ea2fe84c08dc584
Emanuel <Emanuel@Emanuels-MacBook-Pro.local>
2022-12-04 23:11:29 +0200

    l106

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict";
+// Lesson 106
+
+// Rest Patern = basically collects the elements that are unused in the destructuring assignment.
+
+// SPREAD, because on RIGHT side of =
+// const arr = [1,2 ...[3, 5]];
+
+// REST, because on LEFT side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+
+const x = [23, 5, 7];
+add(...x);
